706bd62ff282ffcc6b30db1ef68ef4cedc53481e
Chun Yi <8.5by11@gmail.com>
2022-01-11 17:31:41 -0500

    commit tree 2687b57e06230ddb4ee5e66503b0bbcf8bd42549

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;  
+};
+
+saturdayFun();
+saturdayFun('bathe my dog');
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+mondayWork();
+
+
+function wrapAdjective(visualFlair = '*') {
+    const innerFunction = function(adjective = 'special') {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    }
+    return innerFunction;
+}
+
+wrapAdjective('+')('a dedicated programmer');
